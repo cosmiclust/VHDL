564ebd31479e22ea7ea0e068bf2cf992c79b0cbb
महिमा <93143005+cosmiclust@users.noreply.github.com>
2025-04-08 09:50:22 +0530

    Create AND_process.vhdl

---
+++ AND_process.vhdl
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC-1164.ALL;
+
+entity and_beh is
+    Port( a: in STD_LOGIC;
+          b: in  STD_LOGIC;
+          c: out STD_LOGIC);
+end and_beh;
+
+architecture Behavioral of and_beh is
+begin
+process(a,b)
+begin
+  c <= a and b;
+end process;
+end Behavioral;
