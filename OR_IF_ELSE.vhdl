4c86a29fe9c3e7f1079affe820ed34feb00aeac4
महिमा <93143005+cosmiclust@users.noreply.github.com>
2025-04-08 10:01:34 +0530

    Create OR_IF_ELSE.vhdl

---
+++ OR_IF_ELSE.vhdl
@@ -0,0 +1,10 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity test_or_beh is
+  Port ( a: in STD_LOGIC;
+         b: in STD_LOGIC;
+         c: in STD_LOGIC);
+  end test_or_beh
+
+architecture 
