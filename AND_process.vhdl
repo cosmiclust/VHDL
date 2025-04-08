5d10ea1c34ca2946382970107dfb2014e661849d
महिमा <93143005+cosmiclust@users.noreply.github.com>
2025-04-08 09:54:01 +0530

    Update AND_process.vhdl

--- AND_process.vhdl
+++ AND_process.vhdl
@@ -1,13 +1,13 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity and_beh is
+entity and_beh is       -- entity defines input & output        
     Port( a: in STD_LOGIC;
           b: in  STD_LOGIC;
           c: out STD_LOGIC);
 end and_beh;
 
-architecture Behavioral of and_beh is
+architecture Behavioral of and_beh is  -- architecture defines internal logic
 begin
 process(a,b)
 begin
