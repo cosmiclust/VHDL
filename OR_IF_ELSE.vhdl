8f60a7d833a86d874aedbdd03868b8dcb3a8c380
महिमा <93143005+cosmiclust@users.noreply.github.com>
2025-04-08 12:07:29 +0530

    Update OR_IF_ELSE.vhdl

--- OR_IF_ELSE.vhdl
+++ OR_IF_ELSE.vhdl
@@ -1,10 +1,20 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity test_or_beh is
-  Port ( a: in STD_LOGIC;
-         b: in STD_LOGIC;
-         c: in STD_LOGIC);
-  end test_or_beh
+entity or_gate_ifelse is
+    Port ( a : in  STD_LOGIC;
+           b : in  STD_LOGIC;
+           c : out STD_LOGIC);
+end or_gate_ifelse;
 
-architecture 
+architecture Behavioral of or_gate_ifelse is
+begin
+  process(a, b) -- if else doesn't work without process
+  begin
+    if a = '1' or b = '1' then
+      c <= '1';
+    else
+      c <= '0';
+    end if;
+  end process;
+end Behavioral;
